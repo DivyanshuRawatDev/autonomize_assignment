3ed22f88c7f09d0a4e36aca1630ee0f4550af1e0
DivyanshuRawatDev <divyanshu.rawat.dev@gmail.com>
2025-01-10 09:24:07 +0530

    assignment completed

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,9 +11,23 @@ const Home = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const data = useSelector((store) => store.user.autonomize.users);
+  const [sortField, setSortField] = useState("created_at");
 
   const handleClick = () => {
     dispatch(getMutualFriends(data?.username));
+  };
+
+  const handleSort = async (field) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/sorteduser?sortBy=${field}`
+      );
+      setSearchedUsers(response?.data?.data);
+      toast.success("Users sorted successfully");
+    } catch (error) {
+      console.error("Error fetching sorted users:", error.message);
+      toast.error("Failed to sort users");
+    }
   };
 
   const handleSearch = async (username) => {
@@ -28,15 +42,17 @@ const Home = () => {
     }
   };
 
-  const handleDelete=async(username)=>{
+  const handleDelete = async (username) => {
     try {
-      const response = await axios.put(`http://localhost:8080/api/softDelete/${username}`);
+      const response = await axios.put(
+        `http://localhost:8080/api/softDelete/${username}`
+      );
       toast.success(response?.data?.message);
-      navigate("/")
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   console.log(searchedUsers);
   return (
@@ -90,32 +106,81 @@ const Home = () => {
           >
             Get Mutual Friends
           </button>
-          <span>You have {data?.mutualFriends.length} mutual friends</span>
-          <hr/>
-          <button style={{padding:"3px", color:"white", backgroundColor:"red"}} onClick={()=>handleDelete(data?.username)}>Delete User</button>
+          {data?.mutualFriends.length ? (
+            <span>You have {data?.mutualFriends.length} mutual friends</span>
+          ) : null}
+          <hr />
+          <button
+            style={{ padding: "3px", color: "white", backgroundColor: "red" }}
+            onClick={() => handleDelete(data?.username)}
+          >
+            Delete User
+          </button>
         </div>
       </div>
       <div className="second">
-        <div>
-          <input
-            type="text"
-            placeholder="username"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-          />
-          <button onClick={()=>handleSearch(username)}>Search</button>
-        </div>
-        <div style={{marginTop:"20px"}}>
-        {searchedUsers.map((user)=>{
-          return (<>
-          <div style={{display:"flex",alignItems:"center",gap:"10px",marginBottom:"10px", border:"1px solid black"}}>
-            <img width={50} height={50}  style={{borderRadius:"50%"}} src={user?.avatar_url} />
-            <p>{user?.name}</p>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            marginTop: "10px",
+          }}
+        >
+          <div>
+            <input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <button onClick={() => handleSearch(username)}>Search</button>
           </div>
-          </>)
-        })}
+          <div>
+            <label for="sort">Sort By:</label>
+            <select
+              id="sort"
+              value={sortField}
+              onChange={(e) => {
+                setSortField(e.target.value);
+                handleSort(e.target.value);
+              }}
+            >
+              <option value="followers">Followers</option>
+              <option value="following">following</option>
+              <option value="public_gists">Public Gists</option>
+              <option value="public_repos">Public Repos</option>
+              <option value="created_at" selected>
+                created_at
+              </option>
+            </select>
+          </div>
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          {searchedUsers.map((user) => {
+            return (
+              <>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "10px",
+                    marginBottom: "10px",
+                    border: "1px solid black",
+                  }}
+                >
+                  <img
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: "50%" }}
+                    src={user?.avatar_url}
+                  />
+                  <p>{user?.name}</p>
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </div>
